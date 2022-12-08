4ba70386b85e2ce6cefb16fa224fdbdb0051ae26
Salvation Kemuel <skemuel007@gmail.com>
2022-12-09 00:17:04 +0100

    Initial Migration Created

---
+++ src/Infrastructure/Persistence/Migrations/20221208231612_initial_migration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drones",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SerialNumber = table.Column<string>(type: "varchar(100)", nullable: false),
+                    WeightLimit = table.Column<decimal>(type: "decimal(18, 2", nullable: false),
+                    BatteryCapacity = table.Column<int>(type: "int", nullable: false),
+                    Model = table.Column<int>(type: "int", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drones", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drones_SerialNumber",
+                table: "Drones",
+                column: "SerialNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drones");
+        }
+    }
+}
