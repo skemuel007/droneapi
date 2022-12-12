4c3f6ad02015125158aeaf6d21bc3a58c2db1cf7
Salvation Kemuel <skemuel007@gmail.com>
2022-12-12 18:49:42 +0100

    Repository interfaces created

---
+++ src/Core/Application/Contracts/Persistence/IRepository.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+using Domain.Common;
+
+namespace Application.Contracts.Persistence;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+
+    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        string includeString = null,
+        bool disableTracking = true);
+    
+    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        List<Expression<Func<T, object>>> includes = null,
+        bool disableTracking = true);
+
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
