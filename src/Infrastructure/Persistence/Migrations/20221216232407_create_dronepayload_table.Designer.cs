96e77fe7fa7ec08a3f09cd703fe15736b16c9923
Salvation Kemuel <skemuel007@gmail.com>
2022-12-17 00:25:25 +0100

    Drone request payload migration created

--- src/Infrastructure/Persistence/Migrations/20221216232407_create_dronepayload_table.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20221216232407_create_dronepayload_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
@@ -11,9 +12,10 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DronesAppContext))]
-    partial class DronesAppContextModelSnapshot : ModelSnapshot
+    [Migration("20221216232407_create_dronepayload_table")]
+    partial class create_dronepayload_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Persistence/Migrations/20221216232407_create_dronepayload_table.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class create_dronepayload_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DronePayloads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DroneRequestId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MedicationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DronePayloads", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DronePayloads_DroneRequests_DroneRequestId",
+                        column: x => x.DroneRequestId,
+                        principalTable: "DroneRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DronePayloads_Medications_MedicationId",
+                        column: x => x.MedicationId,
+                        principalTable: "Medications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DronePayloads_DroneRequestId",
+                table: "DronePayloads",
+                column: "DroneRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DronePayloads_MedicationId",
+                table: "DronePayloads",
+                column: "MedicationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DronePayloads");
+        }
+    }
+}
