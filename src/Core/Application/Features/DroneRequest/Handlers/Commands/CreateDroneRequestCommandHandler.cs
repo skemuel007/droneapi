817fb855d126cd61646798f2b2154c79f7e6876c
Salvation Kemuel <skemuel007@gmail.com>
2022-12-17 00:18:23 +0100

    Command handler for drone request completed

---
+++ src/Core/Application/Features/DroneRequest/Handlers/Commands/CreateDroneRequestCommandHandler.cs
@@ -0,0 +1,97 @@
+using System.Net;
+using Application.Contracts.Persistence;
+using Application.DTOs.Drone.Validators;
+using Application.DTOs.DroneRequest;
+using Application.DTOs.DroneRequest.Validators;
+using Application.Features.Drone.Request.Commands;
+using Application.Features.DroneRequest.Request.Commands;
+using Application.Responses;
+using AutoMapper;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Features.DroneRequest.Handlers.Commands;
+
+public class CreateDroneRequestCommandHandler : IRequestHandler<CreateDroneRequestCommand, BaseCommandResponse<CreateDroneRequestResponseDto>>
+{
+    private readonly IDroneRequestRepository _droneRequestRepository;
+    private readonly IDronesRepository _dronesRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateDroneRequestCommandHandler(
+        IDroneRequestRepository droneRequestRepository,
+        IDronesRepository dronesRepository,
+        IMapper mapper,
+        IUnitOfWork unitOfWork)
+    {
+        _droneRequestRepository =
+            droneRequestRepository ?? throw new ArgumentNullException(nameof(droneRequestRepository));
+        _dronesRepository = dronesRepository ?? throw new ArgumentNullException(nameof(dronesRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+
+    public async Task<BaseCommandResponse<CreateDroneRequestResponseDto>> Handle(CreateDroneRequestCommand command,
+        CancellationToken cancellationToken)
+    {
+        var validator = new CreateDroneRequestDtoValidator(_dronesRepository, _droneRequestRepository);
+        var validationResult = await validator.ValidateAsync(command.DroneRequestDto);
+
+        if (validationResult.IsValid == false)
+        {
+            return new BaseCommandResponse<CreateDroneRequestResponseDto>()
+            {
+                Message = "Drone request creation validation failed",
+                Errors = validationResult.Errors.Select(v => v.ErrorMessage).ToList(),
+                StatusCode = HttpStatusCode.UnprocessableEntity
+            };
+        }
+
+        var droneStateCheck = await _dronesRepository.FirstOrDefaultAsync(d => d.Id == command.DroneRequestDto.DroneId, disableTracking: true);
+        if (droneStateCheck.State != DroneState.IDLE)
+        {
+            return new BaseCommandResponse<CreateDroneRequestResponseDto>()
+            {
+                Message = "Cannot create drone request for drone not in Idle state",
+                Errors = validationResult.Errors.Select(v => v.ErrorMessage).ToList(),
+                StatusCode = HttpStatusCode.UnprocessableEntity
+            };
+        }
+        
+        // check if drone is already requested and is engaged
+        var droneRequestStatusCheck = await _droneRequestRepository.AnyAsync(d =>
+            d.DroneId == command.DroneRequestDto.DroneId && d.Status == DroneRequestState.ENGAGED);
+
+        if (droneRequestStatusCheck)
+        {
+            return new BaseCommandResponse<CreateDroneRequestResponseDto>()
+            {
+                Message = $"Drone with id {command.DroneRequestDto.DroneId} is already engaged",
+                Errors = validationResult.Errors.Select(v => v.ErrorMessage).ToList(),
+                StatusCode = HttpStatusCode.BadRequest
+            }; 
+        }
+
+        command.DroneRequestDto.Status = DroneRequestState.ENGAGED; // set status to engaged
+
+        var droneRequest = _mapper.Map<Domain.Entities.DroneRequest>(command.DroneRequestDto);
+        droneRequest = await _droneRequestRepository.AddAsync(droneRequest);
+
+        droneStateCheck.State = DroneState.LOADING;
+        _dronesRepository.Update(droneStateCheck);
+
+        await _unitOfWork.CompleteAsync(); // commit transaction
+
+        var droneRequestResponseDto = _mapper.Map<CreateDroneRequestResponseDto>(droneRequest);
+
+
+        return new BaseCommandResponse<CreateDroneRequestResponseDto>()
+        {
+            Message = "Drone request creation Successful",
+            StatusCode = HttpStatusCode.Created,
+            Data = droneRequestResponseDto,
+            Success = true
+        };
+    }
+}
