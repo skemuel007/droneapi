2da8f7e3ed8ebc23368fa8edaac5a4093227437f
Salvation Kemuel <skemuel007@gmail.com>
2022-12-17 00:19:37 +0100

    Drone request api controller to manage RESTful api requests for drones

---
+++ src/Presentation/API/Controllers/DroneRequestController.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Application.DTOs.DroneRequest;
+using Application.Features.DroneRequest.Request.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiVersion("1.0")]
+public class DroneRequestController : BaseController
+{
+    private IMediator _mediator;
+    public DroneRequestController(IMediator mediator)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+    }
+    
+    /// <summary>
+    /// Make drone request
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost(Name = "MakeDroneRequest")]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    public async Task<IActionResult> MakeDroneRequest([FromBody] CreateDroneRequestDto request)
+    {
+        var command = new CreateDroneRequestCommand() { DroneRequestDto = request };
+        var response = await _mediator.Send(command);
+        return StatusCode((int)response.StatusCode, response);
+    }
+}
